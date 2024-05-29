70aaabbb7886ce1b354904062c8537bb0d8352e6
agdc101 <agdc101@gmail.com>
2024-05-29 22:19:15 +0100

    feat: dummy data added

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const dummy_meetups = [
+    {
+        id: "m1",
+        title: "This is a first meetup",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/b/b8/Darby_House%2C_Telford_-_geograph.org.uk_-_864342.jpg",
+        address: "Meetupstreet 5, 12345 Meetup City",
+        description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+    },
+    {
+        id: "m2",
+        title: "This is a second meetup",
+        image:
+        "https://upload.wikimedia.org/wikipedia/commons/b/b8/Darby_House%2C_Telford_-_geograph.org.uk_-_864342.jpg",
+        address: "Meetupstreet 9, 123 Meetup City",
+        description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+    }
+];
+
+export default function HomePage(props) {
+  return (
+    <>
+      <MeetupList meetups={dummy_meetups} />
+    </>
+  );
+}
