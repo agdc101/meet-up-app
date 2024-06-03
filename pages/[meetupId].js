cff17511dc13f1a30aa29c35e7b9bc09fd21178c
agdc101 <agdc101@gmail.com>
2024-06-03 09:07:50 +0100

    feat: next router added to meetups

---
+++ pages/[meetupId].js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+
+function MeetUpDetails() {
+  return (
+    <Fragment>
+        <div>
+            <h1>Meetup Details Page</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Meeting_Place.jpg/800px-Meeting_Place.jpg" alt="A meetup" />
+            <p>Meetup description</p>
+            <address>Meetup address</address>
+        </div>
+    </Fragment>
+  );
+}   
+
+export default MeetUpDetails;
