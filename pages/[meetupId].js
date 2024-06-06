f361f567f3f52fbf466086136e0c856ac6da1ddf
agdc101 <agdc101@gmail.com>
2024-06-06 09:35:38 +0100

    refactor: adding static paths for meet up detail

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -7,4 +7,21 @@ function MeetUpDetails() {
   );
 }   
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/b/b8/Darby_House%2C_Telford_-_geograph.org.uk_-_864342.jpg",
+        title: "First Meetup",
+        address: "Meetupstreet 5, 12345 Meetup City",
+        description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+      }
+    }
+  };
+}
+
 export default MeetUpDetails;
