7580833343a6674bd91e18e73f5fdc167f63651c
agdc101 <agdc101@gmail.com>
2024-06-09 22:44:32 +0100

    feat api meet-up working

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,22 +5,13 @@ async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
-
-    const newMeetup = {
-      title,
-      image,
-      address,
-      description,
-    };
-
     const client = await MongoClient.connect('mongodb+srv://agdc101:Jupiter68-@cluster0.lrcr2ju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     const db = client.db();
     const meetupsCollection = db.collection('meetups');
 
-    const result = await meetupsCollection.insertOne(newMeetup);
+    const result = await meetupsCollection.insertOne(data);
 
-    console.log(newMeetup);
+    console.log(result);
 
     client.close();
 
--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -15,6 +15,7 @@ function NewMeetupPage() {
     });
 
     const data = await response.json();
+    console.log(data);
 
     router.push('/');
   }
