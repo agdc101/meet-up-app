ab317ec96b7f797108bfd15448b013abaa22b98d
agdc101 <agdc101@gmail.com>
2024-06-13 00:03:07 +0100

    feat: data pulled from db in static props

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -3,11 +3,12 @@
 import classes from './MeetupDetail.module.css';
 
 export default function MeetupDetail (props) {
+  console.log(props);
   return (
-    <section class={classes.container}>
+    <section className={classes.container}>
         <div>
             <h1>Meetup Details Page</h1>
-            <img class={classes.img} src={props.image} alt={props.alt} />
+            <img className={classes.img} src={props.image} alt={props.alt} />
             <p>{props.description}</p>
             <address>{props.address}</address>
         </div>
--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,42 +1,50 @@
 
 import MeetupDetail from "../components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from 'mongodb';
 
-function MeetUpDetails() {
+function MeetUpDetails(props) {
+  console.log(props.meetupData);
   return (
-    <MeetupDetail image="https://via.placeholder.com/400" alt="meetup" address="address 5" description="meet up desc" ></MeetupDetail>
+    <MeetupDetail image={props.meetupData.image} title={props.meetupData.title} address={props.meetupData.address} description={props.meetupData.description} ></MeetupDetail>
   );
 }   
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect('mongodb+srv://agdc101:Jupiter68-@cluster0.lrcr2ju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1"
-        }
-      },
-      {
-        params: {
-          meetupId: "m2"
-        }
+    paths: meetups.map(meetup => ({
+      params: {
+        meetupId: meetup._id.toString()
       }
-    ]
+    }))
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect('mongodb+srv://agdc101:Jupiter68-@cluster0.lrcr2ju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  const db = client.db();
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({ _id: new ObjectId(meetupId) });
+  client.close();
+
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/b/b8/Darby_House%2C_Telford_-_geograph.org.uk_-_864342.jpg",
-        title: "First Meetup",
-        address: "Meetupstreet 5, 12345 Meetup City",
-        description: "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!"
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description
       }
     }
   };
