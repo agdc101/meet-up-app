c6422a9204ad1432a07771ea3abc59cdcae0954f
agdc101 <agdc101@gmail.com>
2024-06-19 22:18:39 +0100

    fix: fallback set to blocking

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -18,7 +18,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: {
         meetupId: meetup._id.toString()
